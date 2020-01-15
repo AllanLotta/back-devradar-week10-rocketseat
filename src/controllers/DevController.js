ed22e184e35fbeb49e9773817a8d35c386e74542
Allan Lotta <allanmatheus77@outlook.com>
2020-01-15 14:40:08 -0300

    added search

---
+++ src/controllers/DevController.js
@@ -0,0 +1,44 @@
+const axios = require("axios");
+const Dev = require("../models/Dev");
+const parserStringAsArray = require("../utils/parseStringASArray");
+
+module.exports = {
+  async index(req, res) {
+    const devs = await Dev.find();
+    return res.json(devs);
+  },
+
+  async store(req, res) {
+    const { github_username, techs, latitude, longitude } = req.body;
+
+    let dev = await Dev.findOne({ github_username });
+
+    if (!dev) {
+      const response = await axios.get(
+        `https://api.github.com/users/${github_username}`
+      );
+
+      const { name = login, avatar_url, bio } = response.data;
+
+      const techsArray = parserStringAsArray(techs);
+      console.log(techsArray);
+
+      const location = {
+        type: "Point",
+        coordinates: [longitude, latitude]
+      };
+
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location
+      });
+
+      console.log(dev);
+    }
+    return res.json(dev);
+  }
+};
